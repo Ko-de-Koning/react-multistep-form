4582c9e8ce18d840f77e49f307284f5855bbdb95
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-04-13 13:27:10 +0200

    feat: forms-created-and-styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-import { useMultistepForm } from "./useMultistepForm"
+import { AccountForm } from "./AccountForm";
+import { AddresForm } from "./AddressForm";
+import { UserForm } from "./UserForm";
+import { useMultistepForm } from "./useMultistepForm";
 
 function App() {
-  const { steps, currentStepIndex, step } = useMultistepForm([
-  <div>One</div>,
-  <div>Two</div>,
-])
-  
-  return ( <div 
-  style={{
-    position: "relative",
-    background: "white",
-    border: "1px solid black",
-    padding: "2rem",
-    margin: "1rem",
-    borderRadius: ".5rem",
-    fontFamiliy: "Arial",
-  }}>
-  <form>
-    <div style={{ position: "absolute", top: ".5rem", right: ".5rem"}}>
-    { currentStepIndex + 1 } / { steps.length }
-    </div> 
-    {step}
-    <div style={{ 
-      marginTop: "1rem",
-      display: "flex", 
-      gap: ".5rem",
-      justifyContent: "flex-end",
-  }}>
-  <button>Back</button>
-  <button>Next</button>
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([<UserForm />, <AddresForm />, <AccountForm />]);
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        background: "white",
+        border: "1px solid black",
+        padding: "2rem",
+        margin: "1rem",
+        borderRadius: ".5rem",
+        fontFamiliy: "Arial",
+      }}
+    >
+      <form>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            gap: ".5rem",
+            justifyContent: "flex-end",
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="button" onClick={next}>
+            {" "}
+            {isLastStep ? "Finish" : "Next"}{" "}
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
